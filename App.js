4c42949640b1c51473c16fd7c76ce2f3a342a66f
YudiNamaizawa <yudi.namaizawa@etec.sp.gov.br>
2023-06-01 22:48:27 -0300

    aula 01-06

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Constants from 'expo-constants';
+
 import Apostila_003 from './components/apostila_003';
 import Apostila_004 from './components/apostila_004';
 
+const corApp = '#000';
+
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Apostila_004/>
-      <StatusBar style="auto" />
+      <StatusBar style={corApp === '#000'} />
     </View>
   );
 }
@@ -18,8 +23,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    //paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff',
+    backgroundColor: '#corApp',
     padding: 8,
+    paddingTop: Constants.statusBarHeight,
   },
 });
